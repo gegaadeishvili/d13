1b99488a94908f3c5947660b9b1b37ab8b014016
Your Name <gegaadeishvili926@gmail.com>
2023-06-14 01:03:00 +0400

    second commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let countdown = 10;
+
+function countdownTimer() {
+  if (countdown > 0) {
+    console.log(countdown);
+    countdown--;
+  } else {
+    console.log("Happy New Year!");
+    clearInterval(timer);
+  }
+}
+
+const timer = setInterval(countdownTimer, 1000);
+
+function calculateYearsUntilRetirement(name, age, retirementAge) {
+    const yearsLeft = retirementAge - age;
+  
+    if (yearsLeft > 0) {
+      console.log(`${name} has ${yearsLeft} years left until retirement.`);
+    } else if (yearsLeft === 0) {
+      console.log(`${name} can retire this year!`);
+    } else {
+      console.log(`${name} is already retired.`);
+    }
+  }
+  
+
+  calculateYearsUntilRetirement("gega", 35, 65);
+  calculateYearsUntilRetirement("nika", 50, 60);
+  calculateYearsUntilRetirement("gocha", 70, 65);
